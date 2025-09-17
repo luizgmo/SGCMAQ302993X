4418d4e594a9aa240204ecf27f79ac38dc15b4d9
luizgmo <luiz18212returns@gmail.com>
2025-09-17 09:21:07 -0300

    DataAccessObject comentado

--- src/java/model/framework/DataAccessObject.java
+++ src/java/model/framework/DataAccessObject.java
@@ -11,15 +11,24 @@ import java.util.HashMap;
 import java.util.StringJoiner;
 
 public abstract class DataAccessObject {
+    // nome da tabela no banco de dados relacional à qual este objeto está mapeado
     private String tableEntity;
+    
+    // flag que indica se esta entidade é nova
     private boolean novelEntity;
+    
+    // flag que indica se a entidade foi modificada
     private boolean changedEntity;
+    
+    // mapa que armazena os campos que foram alterados (padrão unit of work)
     private HashMap<String, Object> dirtyFields;
 
     public DataAccessObject(String tableEntity) {
         setTableEntity(tableEntity);
         dirtyFields = new HashMap<>();
+        // inicialmente, toda entidade é considerada nova
         setNovelEntity(true);
+        // inicialmente, nenhuma alteração foi feita
         setChangedEntity(false);
     }
 
@@ -49,23 +58,27 @@ public abstract class DataAccessObject {
 
     protected void setChangedEntity(boolean changedEntity) {
         this.changedEntity = changedEntity;
+        // limpa os campos sujos quando a entidade é marcada como não alterada
         if (this.changedEntity == false) {
             dirtyFields.clear();
         }
     }
     
-//    Unity of Work
+    // adiciona uma alteração a ser persistida (padrão unit of work)
     protected void addChange(String field, Object value) {
         dirtyFields.put(field, value);
+        // marca a entidade como modificada
         setChangedEntity(true);
     }
     
+    // executa operação de insert no banco de dados
     private void insert() throws SQLException {
         String dml = "INSERT INTO " + getTableEntity();
         
         StringJoiner fields = new StringJoiner(", ");
         StringJoiner values = new StringJoiner(", ");
         
+        // constrói dinamicamente a lista de campos e valores
         for (String field: dirtyFields.keySet()) {
             fields.add(field);
             values.add("?");
@@ -78,6 +91,7 @@ public abstract class DataAccessObject {
         Connection con = DataBaseConnections.getInstance().getConnection();
         PreparedStatement pst =  con.prepareStatement(dml);
         
+        // preenche os parâmetros da query com os valores dos campos alterados
         int index = 1;
         for (String field: dirtyFields.keySet()) {
             pst.setObject(index, dirtyFields.get(field));
@@ -92,11 +106,13 @@ public abstract class DataAccessObject {
         DataBaseConnections.getInstance().closeConnection(con);
     }
     
+    // executa operação de update no banco de dados
     private void update() throws SQLException {
         String dml = "UPDATE " + getTableEntity() + " SET ";
         
         StringJoiner changes = new StringJoiner(", ");
         
+        // constrói dinamicamente a cláusula SET
         for(String field: dirtyFields.keySet()) {
             changes.add(field + " = ? ");
         }
@@ -108,6 +124,7 @@ public abstract class DataAccessObject {
         Connection con = DataBaseConnections.getInstance().getConnection();
         PreparedStatement pst =  con.prepareStatement(dml);
         
+        // preenche os parâmetros da query com os valores dos campos alterados
         int index = 1;
         for (String field: dirtyFields.keySet()) {
             pst.setObject(index, dirtyFields.get(field));
@@ -122,6 +139,7 @@ public abstract class DataAccessObject {
         DataBaseConnections.getInstance().closeConnection(con);
     }
     
+    // persiste as alterações no banco de dados (padrão unit of work)
     public void save() throws SQLException {
         if (isChangedEntity()) {
             if (isNovelEntity()) {
@@ -134,6 +152,7 @@ public abstract class DataAccessObject {
         }
     }
     
+    // remove a entidade do banco de dados
     public void delete() throws SQLException {
         String dml = "DELETE FROM " + getTableEntity() + " WHERE " + getWhereClauseForOneEntity();
         
@@ -148,6 +167,7 @@ public abstract class DataAccessObject {
         DataBaseConnections.getInstance().closeConnection(con);
     }
     
+    // carrega os dados da entidade do banco de dados
     public boolean load() throws SQLException {
         boolean resultado;
         
@@ -165,10 +185,12 @@ public abstract class DataAccessObject {
         
         if (resultado) {
             ArrayList<Object> data = new ArrayList();
+            // extrai todos os dados do resultset
             for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                 data.add(rs.getObject(i));
             }
             
+            // preenche o objeto com os dados recuperados
             fill(data);
             setNovelEntity(false);
         }
@@ -176,6 +198,7 @@ public abstract class DataAccessObject {
         return resultado;
     }
     
+    // recupera todas as entidades da tabela
     public <T extends DataAccessObject> ArrayList<T> getAllTableEntities() throws SQLException {
         ArrayList<T> result = new ArrayList<>();
         
@@ -192,10 +215,12 @@ public abstract class DataAccessObject {
         while (rs.next()) {
             ArrayList<Object> data = new ArrayList<>();
             
+            // extrai todos os dados do resultset
             for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                 data.add(rs.getObject(i));
             }
             
+            // cria uma cópia do objeto preenchido com os dados
             result.add(fill(data).copy());
         }
         
@@ -206,9 +231,12 @@ public abstract class DataAccessObject {
         return result;
     }
     
-    // padrao Template Method
+    // método abstrato para definir a cláusula WHERE para uma entidade específica
     protected abstract String getWhereClauseForOneEntity();
+    
+    // método abstrato para preencher o objeto com dados do banco
     protected abstract DataAccessObject fill(ArrayList<Object> data);
+    
+    // método abstrato para criar uma cópia do objeto
     protected abstract <T extends DataAccessObject> T copy();
-
 }
